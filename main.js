da69b3b41e3c6ed7895f4eb85ea1726baeff2e71
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-07-09 18:39:33 +0900

    Find rabbit program

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-const target=document.querySelector('.target');
-const vertical=document.querySelector('.vertical');
-const horizontal=document.querySelector('.horizontal');
-const tag=document.querySelector('.tag');
-
-document.addEventListener('mousemove', (event)=>{
-    const yCoord=event.clientY;
-    const xCoord=event.clientX;
-    target.style.top=`${yCoord}px`;
-    target.style.left=`${xCoord}px`;
-    vertical.style.left=`${xCoord}px`;
-    horizontal.style.top=`${yCoord}px`;
-    tag.innerHTML=`${xCoord}px,${yCoord}px`;
-    tag.style.left=`${xCoord}px`;
-    tag.style.top=`${yCoord}px`;t
+const btn=document.querySelector('button');
+const rabbitLocation=document.querySelector('.rabbit');
+btn.addEventListener('click',()=>{
+    rabbitLocation.scrollIntoView({ behavior:'smooth', block: 'center'});
 })
